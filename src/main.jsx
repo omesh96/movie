32696204de86ea5376c6dd1eae1e64c854a76730
Omesh Dubey <105977022+omesh96@users.noreply.github.com>
2023-06-16 20:18:38 +0530

    home-page-completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
-import { store } from './Store/store.js'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
-   <BrowserRouter>
-   <App />
-   </BrowserRouter>
-  </Provider>
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
